c3a913c60a6fa4ae801b2b143b00938ff61d796c
stcordobay <scordobay@unal.edu.co>
2023-03-23 23:18:34 -0500

    PagWeb OK

--- app/app.py
+++ app/app.py
@@ -1,9 +1,81 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/other')
+def other():
+    return render_template('other.html')
+
+@app.route('/select_option', methods=['POST'])
+def select_option():
+    option = request.form['option']
+    if option == 'option1':
+        return redirect(url_for('option1'))
+    elif option == 'option2':
+        return redirect(url_for('option2'))
+    elif option == 'option3':
+        return redirect(url_for('option3'))
+
+@app.route('/option1')
+def option1():
+    return render_template('option1.html')
+
+@app.route('/option2')
+def option2():
+    return render_template('option2.html')
+
+@app.route('/option3')
+def option3():
+    return render_template('option3.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5004)
+
+
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/other')
+def other():
+    return render_template('other.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
+"""
+"""
+
 from flask import Flask, render_template
 # from .utils.absolute_paths import AbsPaths
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "you-will-never-guess"
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    # index_file = AbsPaths().get_abs_path_file(file_name='index.html')
+    index_file = 'inihome.html'
+    return render_template(index_file)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,4 +90,6 @@ def index():
 #         return render_template(index_file, text=fp.read())
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5006, host="0.0.0.0")
+    app.run(debug=True, port=5000, host="0.0.0.0")
+    
+"""
